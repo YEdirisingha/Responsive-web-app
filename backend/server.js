b81f65d5e1c5d03b4409dc162c76b70f54f52c70
Yasindu Induwara <yasinduedirisingha@gmail.com>
2025-03-08 20:26:57 +0530

    added product.js for manage products

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,23 @@ import { connectDB } from "./config/db.js";
 dotenv.config();
 const app = express();
 
-app.get('/',(req,res)=>
-    {res.send('Server is ready')}
-);
+app.post('/api/products',async (req,res)=>{
+    const product=req.body;
+
+    if(!product.name || !product.price || !product.image ){
+        return res.status(400).json({error:"All fields are required"});
+    }
+
+    const newProduct=new Product(product);
+
+    try{
+        await newProduct.save();
+        res.status(201).json({success:true,data:newProduct});
+    }catch(error){
+        console.log("error in create product ",error.message);
+        res.status(500).json({success:false,message:"Product not created"});
+    }
+});
 
 app.listen(5000, () => {
     connectDB();
