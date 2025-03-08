3a73f3088b063bde527ff09f3d25727654b8da6e
Yasindu Induwara <yasinduedirisingha@gmail.com>
2025-03-08 23:51:04 +0530

    create add function for add products to database using postman

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.js";
 
 dotenv.config();
 const app = express();
 
-app.post('/api/products',async (req,res)=>{
+app.use(express.json());
+//const newLocal = '';
+app.post("/api/products",async (req,res)=>{
     const product=req.body;
 
     if(!product.name || !product.price || !product.image ){
         return res.status(400).json({error:"All fields are required"});
     }
 
-    const newProduct=new Product(product);
+    const newProduct = new Product(product);
 
     try{
         await newProduct.save();
